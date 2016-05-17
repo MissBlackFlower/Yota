5dcc54756c55d63267946525d156ecdbc16117f4
Viktoriia <Babenko_v_v@ukr.net>
2016-05-17 21:03:57 +0300

    upd

--- build/js/app.js
+++ build/js/app.js
@@ -52,5 +52,39 @@ $(document).ready(function() {
   prevArrow: '.slick-prev',
   nextArrow: '.slick-next'
 	});
+
+	$('body').on('click', function(){
+		if ($(window).width() <= 1000) {
+			$('.js-header').removeClass('is-active');
+			$('.js-header-nav').slideUp(200);
+		}
+	});
+
+	$('.js-btn-menu').on('click', function(event){
+		var this_ = $(this),
+			parent = this_.parents('.js-header'),
+			nav = parent.find('.js-header-nav');
+		if (!parent.hasClass('is-active')){
+			parent.addClass('is-active');
+			nav.slideDown(200);
+		}
+		else {
+			parent.removeClass('is-active');
+			nav.slideUp(200);
+		}
+		event.stopPropagation();
+	});
+
+	$(window).resize(function() {
+		if ($(window).width() >= 1000) {
+			// console.log('ll');
+			$('.js-header-nav').slideDown(200);
+		}
+		else {
+			// console.log('ll');
+			$('.js-header-nav').slideUp(200);
+		}
+	});
+
 });
 
