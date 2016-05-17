f171f60ad66546080e16cee34318c2c9bc1ea0ad
Viktoriia <Babenko_v_v@ukr.net>
2016-05-17 17:27:53 +0300

    slick

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
-
-	// go js
-
+	$('.slick__conteiner').slick({
+  dots: true,
+  infinite: true,
+	autoplay: true,
+  speed: 500,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  prevArrow: '.slick-prev',
+  nextArrow: '.slick-next'
+	});
+	$('.forbes__conteiner').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  adaptiveHeight: true,
+  prevArrow: '.slick-prev',
+  nextArrow: '.slick-next'
+	});
+	$('.product__slick').slick({
+  dots: true,
+  infinite: true,
+	autoplay: true,
+  speed: 500,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  prevArrow: '.slick-prev',
+  nextArrow: '.slick-next'
+	});
 });
